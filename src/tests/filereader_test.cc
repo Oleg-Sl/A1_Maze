3285a9994735213f1b5bdcc468d0f4ada5cd2f7d
Alexander <varlybot@student.21-school.ru>
2023-12-27 16:32:35 +0700

    added tests

--- src/tests/filereader_test.cc
+++ src/tests/filereader_test.cc
@@ -1,5 +1,3 @@
-#include "model/maze/filereader.h"
-
 #include <gtest/gtest.h>
 
 #include <cstddef>
@@ -7,20 +5,20 @@
 #include <vector>
 
 #include "model/maze/maze.h"
+#include "model/maze/mazefilemanager.h"
 
 namespace s21 {
 
 const std::string kAssetsDir = std::string(ASSETS_DIR) + "/";
 
 TEST(MazeReader, CorrectFile3x3) {
-  FileReader reader;
   size_t M = 3;
   size_t N = 3;
 
   std::vector<std::vector<Cell>> grid(M, std::vector<Cell>(N, {1, 0, 0, 0}));
 
   Maze test_maze(grid);
-  Maze loaded_maze = reader.loadMaze(kAssetsDir + "maze_3x3.txt");
+  Maze loaded_maze(MazeFileManager::loadMaze(kAssetsDir + "maze_3x3.txt"));
 
   ASSERT_EQ(test_maze.getRows(), loaded_maze.getRows());
   ASSERT_EQ(test_maze.getCols(), loaded_maze.getCols());
@@ -28,41 +26,28 @@ TEST(MazeReader, CorrectFile3x3) {
 }
 
 TEST(MazeReader, CorrectFile50x50) {
-  FileReader reader;
   size_t M = 50;
   size_t N = 50;
 
   Maze test_maze(M, N);
-  Maze loaded_maze = reader.loadMaze(kAssetsDir + "maze_50x50.txt");
+  Maze loaded_maze(MazeFileManager::loadMaze(kAssetsDir + "maze_50x50.txt"));
 
   ASSERT_EQ(test_maze.getRows(), loaded_maze.getRows());
   ASSERT_EQ(test_maze.getCols(), loaded_maze.getCols());
 }
 
 TEST(MazeReader, CorrectFile0x0) {
-  FileReader reader;
-
-  ASSERT_THROW(reader.loadMaze(kAssetsDir + "maze_0x0.txt"),
-               std::invalid_argument);
-}
-
-TEST(MazeReader, ThrowFileNegativeSize) {
-  FileReader reader;
-
-  ASSERT_THROW(reader.loadMaze(kAssetsDir + "maze_negative.txt"),
+  ASSERT_THROW(MazeFileManager::loadMaze(kAssetsDir + "maze_0x0.txt"),
                std::invalid_argument);
 }
 
 TEST(MazeReader, ThrowNotExistFile) {
-  FileReader reader;
-
-  ASSERT_THROW(reader.loadMaze("not_exist_file"), std::invalid_argument);
+  ASSERT_THROW(MazeFileManager::loadMaze("not_exist_file"),
+               std::invalid_argument);
 }
 
 TEST(MazeReader, ThrowFileWithoutSizeLine) {
-  FileReader reader;
-
-  ASSERT_THROW(reader.loadMaze(kAssetsDir + "maze_without_size.txt"),
+  ASSERT_THROW(MazeFileManager::loadMaze(kAssetsDir + "maze_without_size.txt"),
                std::invalid_argument);
 }
 
--- src/tests/mazegenerator_test.cc
+++ src/tests/mazegenerator_test.cc
@@ -1,37 +1,41 @@
-// #include "mazegenerator.h"
+#include "model/maze/mazegenerator.h"
 
-// namespace s21 {
+#include "gtest/gtest.h"
 
-// TEST(MazeGeneration, MazeNxN) {
-//   MazeGenerator gen;
-//   size_t N = 3;
+namespace s21 {
 
-//   Maze test_maze = gen.generateMaze(N, N);
+TEST(MazeGeneratorTest, GenerateMazeValidDimensions) {
+  size_t rows = 5;
+  size_t cols = 5;
+  s21::MazeGenerator generator;
+  s21::Maze maze = generator.generateMaze(rows, cols);
 
-//   ASSERT_EQ(test_maze.getM(), N);
-//   ASSERT_EQ(test_maze.getN(), N);
-// }
+  EXPECT_EQ(maze.getRows(), rows);
+  EXPECT_EQ(maze.getCols(), cols);
+}
 
-// TEST(MazeGeneration, MazeMxN) {
-//   MazeGenerator gen;
-//   size_t M = 4;
-//   size_t N = 3;
+TEST(MazeGeneratorTest, GenerateMazeInvalidDimensions) {
+  s21::MazeGenerator generator;
+  s21::Maze maze = generator.generateMaze(1, 1);
 
-//   Maze test_maze = gen.generateMaze(M, N);
+  EXPECT_EQ(maze.getRows(), 0);
+  EXPECT_EQ(maze.getCols(), 0);
+}
 
-//   ASSERT_EQ(test_maze.getM(), M);
-//   ASSERT_EQ(test_maze.getN(), N);
-// }
+TEST(MazeGeneratorTest, GenerateMazeZeroRows) {
+  s21::MazeGenerator generator;
+  s21::Maze maze = generator.generateMaze(0, 5);
 
-// TEST(MazeGeneration, MazeMxN_2) {
-//   MazeGenerator gen;
-//   size_t M = 3;
-//   size_t N = 4;
+  EXPECT_EQ(maze.getRows(), 0);
+  EXPECT_EQ(maze.getCols(), 0);
+}
 
-//   Maze test_maze = gen.generateMaze(M, N);
+TEST(MazeGeneratorTest, GenerateMazeZeroCols) {
+  s21::MazeGenerator generator;
+  s21::Maze maze = generator.generateMaze(5, 0);
 
-//   ASSERT_EQ(test_maze.getM(), M);
-//   ASSERT_EQ(test_maze.getN(), N);
-// }
+  EXPECT_EQ(maze.getRows(), 0);
+  EXPECT_EQ(maze.getCols(), 0);
+}
 
-// }  // namespace s21
+}  // namespace s21
